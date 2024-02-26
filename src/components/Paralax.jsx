258a62973f6fd0492634c41ebe7ca2131148f575
Akshaysuresh07 <akshaysureshm010@gmail.com>
2024-02-27 01:29:34 +0530

    image slider

--- src/components/Paralax.jsx
+++ src/components/Paralax.jsx
@@ -6,7 +6,6 @@ import Titles from './Titles';
 const Paralax = () => (
     <Parallax className='paralimage' bgImage={image} strength={800}>
     <Titles/>
-    <h2 style={{marginTop:''}}  className='fst-italic fs-1 text-warning text-center  mt-5' > What we drive says a lot about who we are</h2>
     </Parallax>
 );
 
